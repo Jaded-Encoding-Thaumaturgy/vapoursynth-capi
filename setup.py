bd49da1824b4bf6b98165a76fce22f7de6c9351d
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2024-05-23 12:46:57 +0200

    Bump python

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setuptools.setup(
         'Contact': 'https://discord.gg/qxTxVJGtst',
     },
     install_requires=requirements,
-    python_requires='>=3.11',
+    python_requires='>=3.12',
     packages=[
         package_name
     ],
